00f228ef7de909f9e02bdcdce19d2feaf8aa2d4d
dimitri98713 <chunned@hotmail.com>
2019-06-17 13:59:18 -1000

    Fichiers ajoutes

---
+++ point.py
@@ -0,0 +1,11 @@
+def point(x,y):
+	return [x,y]
+	
+	def getx(p):
+		return p[0]
+	
+	def gety(p):
+		return p[1]
+
+
+
--- test_point_creation.py
+++ test_point_creation.py
@@ -1,23 +1,9 @@
+from point import *
 def test_point_creation():
-	    p = point(22,7)
+	p = point(22,7)
 	
 	def test_access_x_and_y():
-	    assert 22 == getx(p)
-	    assert 7 == gety(p)
+		assert 22 == getx(p)
+		assert 7 == gety(p)
 
-Rouge
-
-Lancez les tests: nosetests3 Constatez qu’ils échouent commitez !
-Implémentation
-
-def point(x,y):
-	    return [x,y]
-	
-	def getx(p):
-	    return p[0]
-	
-	def gety(p):
-	    return p[1]
-
-Quelques tests Utilisons des assertions simples pour le moment. def test_point_creation(): p = point(22,7) def test_access_x_and_y(): assert 22 == getx(p) assert 7 == gety(p)
 
